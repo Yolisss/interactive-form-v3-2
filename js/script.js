a64aaba3f2901661c7aa9307379bcc6ad49d61c7
Yolis <yolisisabel28@gmail.com>
2023-11-02 20:59:17 -0700

    val updated along w the options color menu working

--- js/script.js
+++ js/script.js
@@ -1,20 +1,16 @@
-console.log('test');
 
 //grabbing user input for name
 let name = document.getElementById('name');
-console.log(name);
 name.focus();
 
 //grabbing select elem for job roles 
 //parent elem for all option elems
 let jobRole = document.getElementById('title');
-    console.log(jobRole, 'jobrole');
 //grabbing 'other-job' value from input elem w 'other-job-role'
 let otherJobElem = document.getElementById('other-job-role');
 //seperating var to 1. properly grab the elem
 //2. then apply display 'none' 
 otherJobElem.style.display = 'none';
-    console.log(otherJobElem, 'other')
 
 //adding event listener to job role since it contains 
 //all the options 
@@ -28,5 +24,43 @@ jobRole.addEventListener('change', (e) => {
         otherJobElem.style.display = 'block';
     } else {
         otherJobElem.style.display = 'none';
+    }
+})
+
+//t-shirt info section 
+
+//design select elem
+let design = document.getElementById('design');
+//color select elem
+let color = document.getElementById('color');
+//indiv color options
+let colorOptions = color.children; 
+
+//drop down list is disabled (removed/not shown)
+color.disabled = true; 
+
+design.addEventListener('change', (e) =>{
+    //ex. e => i love JS Puns 
+
+    //looping through each option 
+    for(let i = 0; i < colorOptions.length; i++){
+        //once I select an option, show me drop down list
+         color.disabled = false; 
+         
+        let eValue = e.target.value;
+
+         //going through each options data theme attribute 
+        let dataThemeAttrib = colorOptions[i].getAttribute(["data-theme"]);
+        console.log(dataThemeAttrib);
+
+        //if JS Puns = JS Puns
+        if(eValue === dataThemeAttrib){
+            //will show JS Puns options
+            colorOptions[i].hidden = false; 
+            colorOptions[i].selected = true;
+        } else {
+            colorOptions[i].hidden = true; 
+            colorOptions[i].selected = false;
+        }
     }
 })
